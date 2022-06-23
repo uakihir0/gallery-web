704c238af3adae4833c141e8e418aa69c9a4df28
Akihiro Urushiara <a.urusihara@gmail.com>
2022-06-24 02:21:38 +0900

    add tweet detail page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,26 @@
 import { useEffect, useState } from "react";
-import { Box, Flex, Text, Center } from "@chakra-ui/react";
+import {
+  Box,
+  Text,
+  Center,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
 import StackGrid from "react-stack-grid";
 
-import { Tweets } from "../interactors/type";
+import { Tweet, Tweets } from "../interactors/type";
 import { getTweets } from "../interactors/client/getTweets";
 import { ShadowBox } from "../components/ShadowBox";
+import { TweetDetail } from "../components/TweetDetail";
 
 const Gallery = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const [tweets, setTweets] = useState<Tweets | null>(null);
+  const [tweet, setTweet] = useState<Tweet | null>(null);
 
   useEffect(() => {
     getTweets().then((s) => setTweets(s));
@@ -40,10 +53,17 @@ const Gallery = () => {
           >
             {tweets.tweets.map((tweet) => {
               const zoom = 300 / tweet.imageSize.width;
+              const height = tweet.imageSize.height * zoom;
               return (
                 <div key={tweet.galleryId}>
                   <ShadowBox>
-                    <Box h={tweet.imageSize.height * zoom}>
+                    <Box
+                      h={height}
+                      onClick={() => {
+                        setTweet(tweet);
+                        onOpen();
+                      }}
+                    >
                       <img src={tweet.imageUrl} />
                     </Box>
                   </ShadowBox>
@@ -53,6 +73,19 @@ const Gallery = () => {
           </StackGrid>
         </Box>
       )}
+
+      <Drawer onClose={onClose} isOpen={isOpen} size="lg">
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerBody>
+            <Center my={8}>
+              {/** ツイート本文を表示 */}
+              {tweet && <TweetDetail tweet={tweet} />}
+            </Center>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
     </>
   );
 };
